685b8819739f4f56ea7171148977cfa4de4b2d4b
Jean-Baptiste Bal <Jean-Baptiste@pc15.home>
2014-08-06 10:24:39 +0200

    New pixel perfect icons

--- prototype/conf.lua
+++ prototype/conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
 	t.window.title = "Lakka"
-	t.window.width = 1920
-	t.window.height = 1080
-	t.window.fullscreen = false
+	t.window.width = 1440
+	t.window.height = 900
+	t.window.fullscreen = true
 	t.modules.physics = false
 	t.modules.mouse = true
 end
